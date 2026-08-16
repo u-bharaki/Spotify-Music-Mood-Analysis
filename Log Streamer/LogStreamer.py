7cb83d95ad37dd1478998866c09f092413cb0287
Duygu Akman <duyguaakman@gmail.com>
2026-08-16 23:34:01 +0300

    anlık genişletilmiş

--- Log Streamer/LogStreamer.py
+++ Log Streamer/LogStreamer.py
@@ -4,14 +4,20 @@ import json
 import pandas as pd
 from kafka import KafkaProducer
 import redis
-from datetime import datetime
 
 CURRENT_DIR = os.path.dirname(__file__)
-CSV_PATH = os.path.abspath(os.path.join(CURRENT_DIR, '..', 'Datasets', 'cleaned_streaming_history.csv'))
-
+CSV_PATH = os.path.abspath(
+    os.path.join(CURRENT_DIR, '..', 'Datasets', 'cleaned_streaming_history.csv')
+)
 
 def start_streaming():
-    r = redis.Redis(host='redis', port=6379, db=0, decode_responses=True)
+    r = redis.Redis(
+        host='redis',
+        port=6379,
+        db=0,
+        decode_responses=True
+    )
+
     if not r.exists('system:eps'):
         r.set('system:eps', 5)
 
@@ -21,18 +27,26 @@ def start_streaming():
     )
 
     print(f"Log verisi okunuyor: {CSV_PATH}")
+
     df = pd.read_csv(CSV_PATH)
 
     print("Kafka'ya gerçek zamanlı log akışı başladı! (Durdurmak için Ctrl+C)")
 
     while True:
         for _, row in df.iterrows():
+
             log_dict = row.to_dict()
 
-            # Eski tarihi silip, logun Kafka'ya gönderildiği "şu anki" zamanı atıyoruz
-            log_dict['ts'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            if pd.isna(log_dict.get('ts')):
+                print("Timestamp bulunamadı, kayıt atlanıyor.")
+                continue
 
-            producer.send('vibestream_logs', value=log_dict)
+            log_dict['ts'] = str(log_dict['ts'])
+
+            producer.send(
+                'vibestream_logs',
+                value=log_dict
+            )
 
             try:
                 current_eps = float(r.get('system:eps'))
